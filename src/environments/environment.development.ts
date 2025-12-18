6677b9d77d85dd0972c19b4b123a798ad7638757
wfanabila <wfnabila77@gmail.com>
2025-12-19 07:12:21 +0800

    updated .gitignore

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://vmlcqxqowoiggwcwtbsa.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtbGNxeHFvd29pZ2d3Y3d0YnNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNjg0MzEsImV4cCI6MjA4MTY0NDQzMX0.WrP5LAVJqYOjr_K_Y7uqowTwVQZewVqkvyZAwLtufZ8'
+};
